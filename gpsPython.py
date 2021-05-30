c9a64005cc4afb7560c89e8dae875825c9464494
Breno Minzon <breno.minzon@gmail.com>
2021-05-30 11:38:30 -0400

    Remove unecessary print

--- gpsPython.py
+++ gpsPython.py
@@ -53,27 +53,30 @@ def storeIntoDb(lat, latDir, lng, lngDir, fixTimeString, speed):
     else:
         dblib.saveDistance("{:.4f}".format(0))
 
+    print("Coordinate: " + str(lat) + "," + str(lng))
+    print("Speed: " + str(speed))
+    print("Time Fix (UTC): " + str(fixTimeString))
     previous = actual
 
 
 def printRMC(lines):
-    print("========================================RMC========================================")
+    #  print("========================================RMC========================================")
     fixTime = ''
     fixTime = ''.join(
         getTime(lines[1] + lines[9], "%H%M%S.%W%d%m%y", "%a %b %d %H:%M:%S %Y"))  # added a %W to ignore 00  str
-    print("Fix taken at:" + fixTime, "UTC")
-    print("Status (A=OK,V=KO):", lines[2])
+    #  print("Fix taken at:" + fixTime, "UTC")
+    #  print("Status (A=OK,V=KO):", lines[2])
     latlng = getLatLng(lines[3], lines[5], fixTime)
-    print("Lat,Long: ", latlng[0].replace('-', ''), lines[4], ", ", latlng[1].replace('-', ''), lines[6], sep='')
-    print("Ground Speed (knots):", lines[7])
-    print("Track angle (deg):", lines[8])
-    print("Magnetic variation: ", lines[10], end='')
+    #  print("Lat,Long: ", latlng[0].replace('-', ''), lines[4], ", ", latlng[1].replace('-', ''), lines[6], sep='')
+    #  print("Ground Speed (knots):", lines[7])
+    #  print("Track angle (deg):", lines[8])
+    #  print("Magnetic variation: ", lines[10], end='')
     # The returned string will be either 12 or 13 - it will return 13 if NMEA standard used is above 2.3
-    if len(lines) == 13:
-        print(lines[11])
-        print("Mode (A=Autonomous, D=Differential, E=Estimated, N=Data not valid):", lines[12].partition("*")[0])
-    else:
-        print(lines[11].partition("*")[0])
+    #  if len(lines) == 13:
+        #  print(lines[11])
+        #  print("Mode (A=Autonomous, D=Differential, E=Estimated, N=Data not valid):", lines[12].partition("*")[0])
+    #  else:
+        #  print(lines[11].partition("*")[0])
 
     speedKnots = round(float(lines[7]), 2)
     speedKph = round(speedKnots * 1.852, 2)
@@ -88,78 +91,80 @@ def printRMC(lines):
 
 
 def printGGA(lines):
-    print("========================================GGA========================================")
+    #  print("========================================GGA========================================")
 
     fixTime = ''
     fixTime = ''.join(getTime(lines[1], "%H%M%S.%f", "%H:%M:%S"))
-    print("Fix taken at:", fixTime, "UTC")
+    #  print("Fix taken at:", fixTime, "UTC")
     latlng = getLatLng(lines[2], lines[4], fixTime)
-    print("Lat,Long: ", latlng[0].replace('-', ''), lines[3], ", ", latlng[1].replace('-', ''), lines[5], sep='')
-    print("Fix quality (0 = invalid, 1 = fix, 2..8):", lines[6])
-    print("Satellites:", lines[7].lstrip("0"))
-    print("Horizontal dilution:", lines[8])
-    print("Altitude: ", lines[9], lines[10], sep="")
-    print("Height of geoid: ", lines[11], lines[12], sep="")
-    print("Time in seconds since last DGPS update:", lines[13])
-    print("DGPS station ID number:", lines[14].partition("*")[0])
+    #  print("Lat,Long: ", latlng[0].replace('-', ''), lines[3], ", ", latlng[1].replace('-', ''), lines[5], sep='')
+    #  print("Fix quality (0 = invalid, 1 = fix, 2..8):", lines[6])
+    #  print("Satellites:", lines[7].lstrip("0"))
+    #  print("Horizontal dilution:", lines[8])
+    #  print("Altitude: ", lines[9], lines[10], sep="")
+    #  print("Height of geoid: ", lines[11], lines[12], sep="")
+    #  print("Time in seconds since last DGPS update:", lines[13])
+    #  print("DGPS station ID number:", lines[14].partition("*")[0])
+    print("Altitude: " + line[9])
+    print(" ")
     dblib.saveAltitude(fixTime, lines[9])
 
 
-def printGSA(lines):
-    print("========================================GSA========================================")
+#  def printGSA(lines):
+    #  print("========================================GSA========================================")
 
-    print("Selection of 2D or 3D fix (A=Auto,M=Manual):", lines[1])
-    print("3D fix (1=No fix,2=2D fix, 3=3D fix):", lines[2])
-    print("PRNs of satellites used for fix:", end='')
-    for i in range(0, 12):
-        prn = lines[3 + i].lstrip("0")
-        if prn:
-            print(" ", prn, end='')
-    print("\nPDOP", lines[15])
-    print("HDOP", lines[16])
-    print("VDOP", lines[17].partition("*")[0])
+    #  print("Selection of 2D or 3D fix (A=Auto,M=Manual):", lines[1])
+    #  print("3D fix (1=No fix,2=2D fix, 3=3D fix):", lines[2])
+    #  print("PRNs of satellites used for fix:", end='')
+    #  for i in range(0, 12):
+    #    prn = lines[3 + i].lstrip("0")
+    #    if prn:
+            #  print(" ", prn, end='')
+    #  print("\nPDOP", lines[15])
+    #  print("HDOP", lines[16])
+    #  print("VDOP", lines[17].partition("*")[0])
 
 
-def printGSV(lines, db):
-    if lines[2] == '1':  # First sentence
-        print("========================================GSV========================================")
-    else:
-        print("===================================================================================")
+#  def printGSV(lines, db):
+   #  if lines[2] == '1':  # First sentence
+        #  print("========================================GSV========================================")
+   #  else:
+        #  print("===================================================================================")
 
-    print("Number of sentences:", lines[1])
-    print("Sentence:", lines[2])
-    print("Satellites in view:", lines[3].lstrip("0"))
-    for i in range(0, int(len(lines) / 4) - 1):
-        print("Satellite PRN:", lines[4 + i * 4].lstrip("0"))
-        print("Elevation (deg):", lines[5 + i * 4].lstrip("0"))
-        print("Azimuth (deg):", lines[6 + i * 4].lstrip("0"))
-        print("SNR (higher is better):", lines[7 + i * 4].partition("*")[0])
+    #  print("Number of sentences:", lines[1])
+    #  print("Sentence:", lines[2])
+    #  print("Satellites in view:", lines[3].lstrip("0"))
+    #  for i in range(0, int(len(lines) / 4) - 1):
+        #  print("Satellite PRN:", lines[4 + i * 4].lstrip("0"))
+        #  print("Elevation (deg):", lines[5 + i * 4].lstrip("0"))
+        #  print("Azimuth (deg):", lines[6 + i * 4].lstrip("0"))
+        #  print("SNR (higher is better):", lines[7 + i * 4].partition("*")[0])
 
 
 def printGLL(lines):
-    print("========================================GLL========================================")
+    #  print("========================================GLL========================================")
 
     fixTime = ''
 
     fixTime = ''.join(getTime(lines[5], "%H%M%S.%f", "%H:%M:%S"))
     latlng = getLatLng(lines[1], lines[3], fixTime)
-    print("Lat,Long: ", latlng[0].replace('-', ''), lines[2], ", ", latlng[1].replace('-', ''), lines[4], sep='')
-    print("Fix taken at:", fixTime, "UTC")
-    print("Status (A=OK,V=KO):", lines[6])
-    if lines[7].partition("*")[0]:  # Extra field since NMEA standard 2.3
-        print("Mode (A=Autonomous, D=Differential, E=Estimated, N=Data not valid):", lines[7].partition("*")[0])
+    #  print("Lat,Long: ", latlng[0].replace('-', ''), lines[2], ", ", latlng[1].replace('-', ''), lines[4], sep='')
+    #  print("Fix taken at:", fixTime, "UTC")
+    #  print("Status (A=OK,V=KO):", lines[6])
+    #  if lines[7].partition("*")[0]:  # Extra field since NMEA standard 2.3
+        #  print("Mode (A=Autonomous, D=Differential, E=Estimated, N=Data not valid):", lines[7].partition("*")[0])
 
 
 def printVTG(lines):
-    print("========================================VTG========================================")
+    #  print("========================================VTG========================================")
 
-    print("True Track made good (deg):", lines[1], lines[2])
-    print("Magnetic track made good (deg):", lines[3], lines[4])
-    print("Ground speed (knots):", lines[5], lines[6])
-    print("Ground speed (km/h):", lines[7], lines[8].partition("*")[0])
+    #  print("True Track made good (deg):", lines[1], lines[2])
+    #  print("Magnetic track made good (deg):", lines[3], lines[4])
+    #  print("Ground speed (knots):", lines[5], lines[6])
+    #  print("Ground speed (km/h):", lines[7], lines[8].partition("*")[0])
     speed = lines[7]
-    if lines[9].partition("*")[0]:  # Extra field since NMEA standard 2.3
-        print("Mode (A=Autonomous, D=Differential, E=Estimated, N=Data not valid):", lines[9].partition("*")[0])
+    #  if lines[9].partition("*")[0]:  # Extra field since NMEA standard 2.3
+        #  print("Mode (A=Autonomous, D=Differential, E=Estimated, N=Data not valid):", lines[9].partition("*")[0])
 
 
 def checksum(line):
@@ -171,16 +176,16 @@ def checksum(line):
     try:  # Just to make sure
         inputChecksum = int(checkString[2].rstrip(), 16);
     except:
-        print("Error in string")
+        #  print("Error in string")
         return False
 
     if checksum == inputChecksum:
         return True
     else:
-        print("=====================================================================================")
-        print("===================================Checksum error!===================================")
-        print("=====================================================================================")
-        print(hex(checksum), "!=", hex(inputChecksum))
+        #  print("=====================================================================================")
+        #  print("===================================Checksum error!===================================")
+        #  print("=====================================================================================")
+        #  print(hex(checksum), "!=", hex(inputChecksum))
         return False
 
 
