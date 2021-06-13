d1cc2ff531ca1075d3aeb0fb759b9047a2e4b8e9
Breno Minzon <breno.minzon@gmail.com>
2021-06-12 22:00:40 -0400

    Add duplicated entry to test failover scenario

--- pyTest.py
+++ pyTest.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     dblib.startNewRoute()
     dblib.saveCoordinate('2021-06-07 00:14:28', '45.27147', '-75.88351', 1.23)
     dblib.saveAltitude('2021-06-07 00:14:28', 100.2, 8)
+    dblib.saveCoordinate('2021-06-07 00:14:28', '45.27147', '-75.88351', 1.23)
+    dblib.saveAltitude('2021-06-07 00:14:28', 100.2, 8)
     dblib.saveCoordinate('2021-06-07 00:14:29', '45.27145', '-75.88350', 1.24)
     dblib.saveDistance('45.27145', '-75.88350')
     dblib.saveAltitude('2021-06-07 00:14:29', 101.2, 10)
